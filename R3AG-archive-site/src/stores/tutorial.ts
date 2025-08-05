8356601e69a7f26e50504bd516615bb9b917cd6f
artorias <fyb0118@gmail.com>
2025-08-05 15:55:43 +0900

    feat: fix url.

--- R3AG-archive-site/src/stores/tutorial.ts
+++ R3AG-archive-site/src/stores/tutorial.ts
@@ -1,9 +1,18 @@
 import { defineStore } from 'pinia'
 import data from '../data/data.json'
 
+interface Tutorial {
+  id: string;
+  title: string;
+  link: string;
+  date: string;
+  location?: string;
+  description: string;
+};
+
 export const useTutorialStore = defineStore('tutorial', {
   state: () => ({
-    tutorials: [] as typeof data.tutorials
+    tutorials: [] as Tutorial[]
   }),
   actions: {
     loadTutorials() {
